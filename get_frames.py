b75637cda8fbb30f0e86cda4fad526f6e6718467
Phillip Jensen <33448819+cryptobench@users.noreply.github.com>
2021-12-23 23:14:51 +0100

    Get proper extension

--- get_frames.py
+++ get_frames.py
@@ -4,4 +4,4 @@ scene = bpy.context.scene
 out = scene.frame_end - scene.frame_start + 1
 print("[" + str(out) + "]")
 print("|" + str(scene.frame_start) + "|")
-print("%" + str(bpy.context.scene.render.image_settings.file_format) + "%")
+print("%" + str(bpy.context.scene.render.file_extension) + "%")
